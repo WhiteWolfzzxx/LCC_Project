6a804b23e2c3cf993e54fca4a3ce2644180ea16a
whitewolfzzxx <mboyandsboy@gmail.com>
2017-03-30 22:11:40 -0400

    Created project

---
+++ LCC_Project/LCC_Project/LCC_Project.cpp
@@ -0,0 +1,14 @@
+// LCC_Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "HELLO WORLD!" << endl;
+	system("pause");
+    return 0;
+}
+
